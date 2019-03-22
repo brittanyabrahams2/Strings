e952dabf9280143dc86b5f348329091eece721d9
brittanyabrahams2 <43973188+brittanyabrahams2@users.noreply.github.com>
2019-03-22 16:40:56 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        String myString = "Don't play with me";
+        System.out.println(myString);
+        myString = myString + ",I suck at soccer";
+        System.out.println(myString);
+
+        String numberString = "250";
+        numberString = numberString + "69";
+        System.out.println(numberString);
+
+        String laststring= "10";
+        int intVal = 40;
+        laststring = laststring + intVal;
+        System.out.println(laststring);
+
+
+    }
+}
